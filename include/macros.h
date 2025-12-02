8d16a56255a3ffc623fd926db2de9b2afdc6a1e1
memory5ty7 <amaury.willem57@gmail.com>
2025-12-02 21:29:45 +0100

    min, max, nelems macros

---
+++ include/macros.h
@@ -0,0 +1,9 @@
+#ifndef POKEPLATINUM_MACROS_H
+#define POKEPLATINUM_MACROS_H
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+#define NELEMS(x) (sizeof(x) / sizeof((x)[0]))
+
+#endif // POKEPLATINUM_MACROS_H
